020d4f369b5dcab369bb59a8232041d015fe4257
MilaBozhevskaya <mi.bozhev@gmail.com>
2024-07-22 15:40:22 +0300

    chore: set environment

---
+++ module.d.ts
@@ -0,0 +1,6 @@
+declare module "*.css";
+declare module "*.scss";
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpg";
+declare module "*.jpeg";
---
+++ src/app/app.tsx
@@ -0,0 +1,4 @@
+import type { FC } from "react";
+import "./styles.scss";
+
+export const App: FC = () => <div>App</div>;
